3fe4c1d7dc9a6f1e9c64552589641fb1edebed3d
silveiraTF <silveiraforneli@gmail.com>
2021-12-22 14:53:03 -0300

    studying

--- 3testevue/src/components/appGit.vue
+++ 3testevue/src/components/appGit.vue
@@ -1,41 +1,40 @@
 <template>
-  <div class="Cards grid">
-    <Card style="width: 45rem" class="col-6 col-offset-3">
-      <template #header>
-        <h2>GitHub API Project</h2>
-        <h3>Search your repository</h3>
-      </template>
-      <template #content>
-        <label for="user" style="font-size: 1.75rem; font-family: monospace"
-          >User</label
-        >
-        <InputText
-          style="width: 250px; margin-left: 18%"
-          id="user"
-          type="text"
-          placeholder="Enter your username..."
-          v-model="userSearch"
-          class="field"
-        />
-      </template>
-      <template #footer>
-        <Button
-          class="button"
-          icon="pi pi-search"
-          type="button"
-          value="Submit"
-          label="Search"
-          style="width: 110px; margin-left: 41%"
-          @click.prevent.stop="onGetUser()"
-        />
-      </template>
-    </Card>
-    <div>
-      <DataTable
-        :value="repos"
-        responsiveLayout="scroll"
-        style="width: 46%; margin-left: 24%"
-      >
+  <div class="flex flex-column m-5 align-items-center">
+    <div
+      class="absolute top-0 left-0 px-4 py-3 w-full font-bold bg-blue-500 text-white"
+    >
+      TESTE | GIT
+    </div>
+    <div
+      class="absolute left-0 px-4 py-3 w-full font-bold bg-white text-blue-500"
+      style="margin-top: 10px"
+    >
+      Search your repository
+    </div>
+
+    <div
+      class="flex flex-column p-card p-5 mb-5 mt-8 fadeinleft animation-duration-1000"
+    >
+      <label for="user">User</label>
+      <InputText
+        id="user"
+        type="text"
+        placeholder="Enter your username..."
+        v-model="userSearch"
+        class="mb-5"
+      />
+      <Button
+        class="button"
+        icon="pi pi-search"
+        type="button"
+        value="Submit"
+        label="Search"
+        @click.prevent.stop="onGetUser()"
+      />
+    </div>
+
+    <div class="p-card p-5 w-9">
+      <DataTable :value="repos" responsiveLayout="scroll">
         <Column field="name" header="Name"></Column>
         <Column field="full_name" header="Full Name"></Column>
       </DataTable>
